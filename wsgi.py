61ac36d49bb0dfa0276a47e865aab172c1c0d3c0
devruji <en.rujikorn@gmail.com>
2022-07-10 15:00:14 +0700

    add swagger.yml to app

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 from routes.route import app
 
+# TODO: Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
